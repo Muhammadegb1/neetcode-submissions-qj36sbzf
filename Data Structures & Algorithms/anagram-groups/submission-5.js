d2df399e6043f2a19ec013d2d4f2b256c77d6705
Muhammad Egbaria <156935046+Muhammadegb1@users.noreply.github.com>
2026-04-15 21:08:49 +0300

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map();
+
+        for (let i =0; i< strs.length; i++){
+            const arr = new Array(26).fill(0);
+
+            for (const tav of strs[i]){
+                arr[tav.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+            }
+            const key = arr.join("#");
+            if (!map.has(key)){
+                map.set(key, []);
+            }
+            map.get(key).push(strs[i])
+        }
+
+        const arr = [];
+        for(const [key , value] of map.entries()){
+            arr.push(value);
+        }
+        return arr;
+    }
+}
