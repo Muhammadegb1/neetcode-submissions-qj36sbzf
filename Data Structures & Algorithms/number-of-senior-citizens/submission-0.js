154a87e909a70853c449aa2040212e8acd75ac74
Muhammad Egbaria <156935046+Muhammadegb1@users.noreply.github.com>
2026-08-15 22:22:49 +0300

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string[]} details
+     * @return {number}
+     */
+    countSeniors(details) {
+        let count = 0;
+        for(const value of details ){
+            const age = Number(value.substring(11,13))
+            if(age > 60){
+                count++;
+            }
+        }
+        return count;
+    }
+}
